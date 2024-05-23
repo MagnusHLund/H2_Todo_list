1d1270cbe9d4b94fed3b522b8b83b334c9923293
Magnus Lund <124877369+MagnusHLund@users.noreply.github.com>
2024-05-23 14:07:09 +0200

    fixed routes

--- Web_client/src/App.tsx
+++ Web_client/src/App.tsx
@@ -1,15 +1,17 @@
 import Navbar from './Components/Content/Navbar'
 import TodoList from './Components/Routes/TodoList'
 import HomePage from './Components/Routes/HomePage'
-import { Route } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 
 function App() {
   return (
     <>
       <Navbar />
-      <Route path="List" element={<TodoList />} />
-      <Route path="Home" element={<HomePage />} />
-      <Route path="*" element={<HomePage />} />
+      <Routes>
+        <Route path="List" element={<TodoList />} />
+        <Route path="Home" element={<HomePage />} />
+        <Route path="*" element={<HomePage />} />
+      </Routes>
     </>
   )
 }
