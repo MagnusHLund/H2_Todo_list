39667e0a6c1e97ba5e814833fe8683dcca7a0e73
Magnus Lund <124877369+MagnusHLund@users.noreply.github.com>
2024-05-23 13:22:58 +0200

    Added functionality for input fields

--- Web_client/src/Components/Input/TextInput.tsx
+++ Web_client/src/Components/Input/TextInput.tsx
@@ -1,9 +1,21 @@
 import StandardInputProps from './StandardInputProps'
+import './TextInput.scss'
 
-interface TextInputProps extends StandardInputProps {}
+interface TextInputProps extends StandardInputProps {
+  text?: string
+  className?: string
+}
 
-const TextInput: React.FC<TextInputProps> = () => {
-  return <></>
+const TextInput: React.FC<TextInputProps> = ({
+  text = '',
+  className = '',
+  onChange,
+}) => {
+  return (
+    <textarea className={`text-input ${className}`} onChange={onChange}>
+      {text}
+    </textarea>
+  )
 }
 
 export default TextInput
